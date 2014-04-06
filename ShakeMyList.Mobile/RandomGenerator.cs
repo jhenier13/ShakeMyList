dd127a228a07d4cc36e005723e447b7f617ca892
jhenier13 <jhe.nier13@gmail.com>
2014-04-06 11:21:12 -0400

    Archivo faltante
    Agregado RandomGenerator

---
+++ ShakeMyList.Mobile/RandomGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShakeMyList.Mobile
+{
+    public static class RandomGenerator
+    {
+        private static Random __generator;
+
+        static RandomGenerator()
+        {
+            __generator = new Random();
+        }
+
+        public static int GenerateInteger(int min, int max)
+        {
+            int randomInteger = __generator.Next(min, max); 
+            return randomInteger;
+        }
+    }
+}
+
